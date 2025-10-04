97abc2980dbaa4dc03a0f86679821a164b3b797b
James Hirschorn <james.hirschorn@quantitative-technologies.com>
2025-10-04 09:32:49 +0700

    enhancement(examples,rest_api_async): renew_listen_key: proper demonstration

--- examples/rest_api_async/data_stream_listen_key/renew_listen_key.py
+++ examples/rest_api_async/data_stream_listen_key/renew_listen_key.py
@@ -3,14 +3,19 @@ import logging
 import asyncio
 from aster.rest_api import AsyncClient
 from aster.lib.utils import config_logging
+from dotenv import load_dotenv
+import os
 
 
 async def main():
+    load_dotenv()
     config_logging(logging, logging.DEBUG)
-    key = ""
+    key = os.getenv("ASTER_KEY")
     client = AsyncClient(key, base_url="https://fapi.asterdex.com")
     try:
-        logging.info(await client.renew_listen_key(""))
+        listen_key = await client.new_listen_key()
+        renewed_key = await client.renew_listen_key(listen_key)
+        logging.info(f"Listen key renewed successfully: {renewed_key}")
     finally:
         await client.close()
 
